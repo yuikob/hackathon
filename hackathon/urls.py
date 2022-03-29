17cbc65ba33121b272295712593a3820b613da52
kem <kentarou524@gmail.com>
2022-03-29 12:01:08 +0900

    変更しました

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+#URL情報まとめ
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('pochi.urls')),
 ]
+
