797f748e12db42b0c165c1af71849a8823875716
yuikob <yui.kobayashi@milk-med.com>
2022-03-29 16:36:50 +0900

    ページを増やしました

--- pochi/urls.py
+++ pochi/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+from pochi.views import TestView
 
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('pochi', TemplateView.as_view(template_name='pochi.html'), name="pochi"),
+    path('pochi1', TemplateView.as_view(template_name='pochi1.html'), name="pochi1"),
+    path('pochi2', TemplateView.as_view(template_name='pochi2.html'), name="pochi2"),
+    path('pochi3', TemplateView.as_view(template_name='pochi3.html'), name="pochi3"),
+
+    path('test', TestView.as_view(), name="test"),
 ]
